25c7d4441c67408ef6dae58a02668df87b66ee6f
Bekzhan Bubakanov <b.bubakanov@gmail.com>
2022-02-06 15:30:46 +0600

    change course.active to not null

--- src/LessonPlanningSystem.DatabaseLayer/Entities/Course.cs
+++ src/LessonPlanningSystem.DatabaseLayer/Entities/Course.cs
@@ -15,7 +15,7 @@ public class Course
     public string Code { get; set; }
     public string Name { get; set; }
     public int Credits { get; set; }
-    public bool? Active { get; set; }
+    public bool Active { get; set; }
     public int SubgroupMode { get; set; }
     public int UnpositionedTheoryHours { get; set; }
     public int UnpositionedPracticeHours { get; set; }
